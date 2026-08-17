8cf92b5423292bfcb3cbe6f92e6025b4edc8ac51
Raveendra G <raveendra1434@gmail.com>
2026-08-17 00:50:14 -0700

    Prepare server for production deployment

--- server/server.js
+++ server/server.js
@@ -2256,8 +2256,8 @@ personalVoiceFile = req.file.filename;
     }
   }
 );
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Truvora server running on port ${PORT}`);
 });
