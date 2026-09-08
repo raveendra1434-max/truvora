98da6585273a8d0cccb16a719774c7a6d6accbc8
Raveendra G <raveendra1434@gmail.com>
2026-09-08 04:46:16 -0700

    Fix Firebase configuration

--- client/firebase.js
+++ client/firebase.js
@@ -15,14 +15,22 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDuqAZRS86CP-upVTglCUj8VSvXo9tm7tY",
+  authDomain: "truvora-7d3c7.firebaseapp.com",
+  projectId: "truvora-7d3c7",
+  storageBucket: "truvora-7d3c7.firebasestorage.app",
+  messagingSenderId: "241553183722",
+  appId: "1:241553183722:web:8bf7e810d7778be2e61893"
 };
+
+// Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
 
