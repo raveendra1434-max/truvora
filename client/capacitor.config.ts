1215c8a173d3d7a8c5fc4cb70a27edbdb35264be
Raveendra G <raveendra1434@gmail.com>
2026-09-16 18:24:41 -0700

    Add Capacitor iOS project

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.trulexity.app',
+  appName: 'Trulexity',
+  webDir: 'build'
+};
+
+export default config;
